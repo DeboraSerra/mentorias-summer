ba805474df5ac6c6afe02e0903cddc81cdb3d286
debora <debora.r.serra@gmail.com>
2022-08-26 18:59:28 -0300

    feat: adiciona lógica de adicionar ao carrinho na página PDP

--- revisao-store-redux/src/pages/PDP.js
+++ revisao-store-redux/src/pages/PDP.js
@@ -2,28 +2,61 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Attributes from '../components/Attributes';
 import Gallery from '../components/Gallery';
-import { fetchDetails } from '../redux/actions';
+import { addToCart, fetchDetails } from '../redux/actions';
 
 class PDP extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      quantity: 0,
+    }
+  }
   componentDidMount() {
     const { dispatch, match: { params: { id } } } = this.props;
     dispatch(fetchDetails(id));
   }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ quantity: +value });
+  }
+
+  handleClick = () => {
+    const { quantity } = this.state;
+    const { dispatch, product } = this.props;
+    dispatch(addToCart({ ...product, qnt: quantity }));
+  }
+
   render() {
     const { loading, product } = this.props;
     console.log({ product, loading });
     const { attributes, available_quantity: availableQnt, pictures, price,
       title, warranty, shipping } = product;
+    const { quantity } = this.state;
     return (
       <main>
         {loading ? <p>Carregando...</p> : (
           <section>
-            <h2>{product.title}</h2>
-            <p>{`Quantidade disponível: ${availableQnt}`}</p>
-            <Gallery photos={ pictures } title={ title } />
-            <p>{`Preço: R$${price}`}</p>
-            {shipping.free_shipping && <p>Frete grátis</p>}
-            <p>{warranty}</p>
+            <section>
+              <h2>{product.title}</h2>
+              <p>{`Quantidade disponível: ${availableQnt}`}</p>
+              <Gallery photos={ pictures } title={ title } />
+              <p>{`Preço: R$${price}`}</p>
+              {shipping.free_shipping && <p>Frete grátis</p>}
+              <p>{warranty}</p>
+            </section>
+            <section>
+              <input
+                type="number"
+                name="quantity"
+                min="0"
+                max={ availableQnt }
+                value={ quantity }
+                onChange={ this.handleChange }
+              />
+              <button type="button" onClick={ this.handleClick }>
+                Adicionar ao carrinho
+              </button>
+            </section>
             <Attributes attributes={ attributes } />
           </section>
         )}
